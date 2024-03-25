b229c702b8c85235879005dc59076d4f7db75890
Tran Ho <tranho@Mac-mini.local>
2024-03-25 14:17:33 -0700

    modified js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const form = document.forms[0];
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const email = form[0].value;
-    console.log(email)
     const errorMsg = document.getElementById("error-message");
     if(errorMsg){
         errorMsg.remove();
