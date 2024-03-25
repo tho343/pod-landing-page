e0a682bf1c1e8ab9deee9c8daa49f266c478462a
Tran Ho <tranho@Mac-mini.local>
2024-03-25 14:16:32 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const form = document.forms[0];
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = form[0].value;
+    console.log(email)
+    const errorMsg = document.getElementById("error-message");
+    if(errorMsg){
+        errorMsg.remove();
+    }
+    if (! email.includes("@")) {
+        form.innerHTML += 
+        `<p id="error-message">Oops! Please check your email</p>`
+    }
+    
+})
